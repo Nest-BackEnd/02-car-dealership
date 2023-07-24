f37e9c09d4e144ba2bc24ca9562eb6d5b537ecef
HectorRoman <hectorr9577@gmail.com>
2023-07-24 16:29:55 -0600

    Añadir data

--- src/brands/brands.service.ts
+++ src/brands/brands.service.ts
@@ -1,9 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid'
 import { CreateBrandDto } from './dto/create-brand.dto';
 import { UpdateBrandDto } from './dto/update-brand.dto';
+import { Brand } from './entities/brand.entity';
 
 @Injectable()
 export class BrandsService {
+
+ private brands: Brand[] = [
+  {
+    id: uuid(),
+    name: 'Toyota',
+    createdAt: new Date().getTime(),
+  }
+ ];
+
   create(createBrandDto: CreateBrandDto) {
     return 'This action adds a new brand';
   }
@@ -12,8 +23,10 @@ export class BrandsService {
     return `This action returns all brands`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} brand`;
+  findOne(id: string) {
+    const brand = this.brands.find(brand => brand.id === id);
+    if(!brand) throw new NotFoundException(`Brand with id: ${id} not found`);
+    return brand;
   }
 
   update(id: number, updateBrandDto: UpdateBrandDto) {
